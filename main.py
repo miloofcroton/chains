c8d2964b224883f2209bad9c657d5277f0d0c4be
Jack Toumey <jack@toumey.io>
2025-11-16 15:39:53 -0700

    running code

--- main.py
+++ main.py
@@ -3,25 +3,27 @@ from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 
 # SECURE THIS KEY!
-api_key = ""
+# api_key = ""
 
-llm = OpenAI(
-    openai_api_key=api_key
-)
+# llm = OpenAI(
+#     openai_api_key=api_key
+# )
 
-code_prompt = PromptTemplate(
-    template="Write a very short {language} function that will {task}",
-    input_variables=["language", "task"]
-)
+# code_prompt = PromptTemplate(
+#     template="Write a very short {language} function that will {task}",
+#     input_variables=["language", "task"]
+# )
 
-code_chain = LLMChain(
-    llm=llm,
-    prompt=code_prompt
-)
+# code_chain = LLMChain(
+#     llm=llm,
+#     prompt=code_prompt
+# )
 
-result = code_chain({
-    "language": "python",
-    "task": "return a list of numbers"
-})
+# result = code_chain({
+#     "language": "python",
+#     "task": "return a list of numbers"
+# })
 
-print(result["text"])
+# print(result["text"])
+
+print("test")
