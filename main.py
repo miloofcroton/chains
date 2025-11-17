8b311ebb6ddec86bbe8b9658d41586be08190446
Jack Toumey <jack@toumey.io>
2025-11-16 21:42:24 -0700

    chaining prompts

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import argparse
 import os
 
 from dotenv import load_dotenv
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SequentialChain
 from langchain.prompts import PromptTemplate
 from langchain_openai import OpenAI
 
@@ -19,22 +19,40 @@ llm = OpenAI(
 )
 
 code_prompt = PromptTemplate(
-  template="Write a very short {language} function that will {task}",
   input_variables=["language", "task"],
+  template="Write a very short {language} function that will {task}",
+)
+test_prompt = PromptTemplate(
+  input_variables=["language", "code"],
+  template="Write a test for the following {language} code:\n{task}",
 )
 
 code_chain = LLMChain(
   llm=llm,
   prompt=code_prompt,
+  output_key="code",
+)
+test_chain = LLMChain(
+  llm=llm,
+  prompt=test_prompt,
+  output_key="test",
 )
 
-result = code_chain(
+chain = SequentialChain(
+  chains=[code_chain, test_chain],
+  input_variables=["task", "language"],
+  output_variables=["test", "code"],
+)
+
+result = chain(
   {
     "language": args.language,
     "task": args.task,
   }
 )
 
-print(result["text"])
-
-# print("test")
+print(">>>>>Generated Code")
+print(result["code"])
+print("\n\n")
+print(">>>>>Generated Test")
+print(result["test"])
